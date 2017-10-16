ad33fe8018a98310da0695595e3c6b3c3b532e3b
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-10-16 10:43:29 -0400

    added scope to replace methods in the model and created some conditional items in the scopes

--- app/models/track.rb
+++ app/models/track.rb
@@ -2,29 +2,42 @@ class Track < ApplicationRecord
 SHORT = 180000
 LONG  = 360000
 
+  # short hand for the below methods -- it is called scope!!!!!! use it !!!!
+  # allows to  write in one line hte below methods
 
-  def self.short
-    shorter_than(SHORT)
-  end
+  scope :short, -> { shorter_than(SHORT) }
+  scope :long, -> { longer_than_or_equal_to(LONG) }
+  scope :medium, -> { longer_than_or_equal_to(SHORT).shorter_than(LONG) }
 
-  def self.long
-    shorter_than(LONG)
-  end
+  # def self.short
+  #   shorter_than(SHORT)
+  # end
+  #
+  # def self.long
+  #   shorter_than(LONG)
+  # end
+  #
+  # def self.medium
+  # longer_than_or_equal_to(SHORT).shorter_than(LONG)
+  # end
 
-  def self.medium
-  longer_than_or_equal_to(SHORT).shorter_than(LONG)
-  end
 
-  def self.shorter_than(milliseconds)
-    where('milliseconds < ?', milliseconds)
-  end
+  scope :shorter_than, -> (milliseconds ) { where('milliseconds < ?', milliseconds) if milliseconds.present? && milliseconds > 0}
 
-  def self.longer_than_or_equal_to(milliseconds)
-    where('milliseconds >= ?', milliseconds)
-  end
+  # def self.shorter_than(milliseconds)
+  #   where('milliseconds < ?', milliseconds)
+  # end
 
-  def self.starts_with(char)
-    where('name ILIKE ?', "#{ char }%")
-  end
+  scope :longer_than_or_equal_to, -> (milliseconds) { where('milliseconds >= ?', milliseconds)}
+
+  # def self.longer_than_or_equal_to(milliseconds)
+  #   where('milliseconds >= ?', milliseconds)
+  # end
+
+
+  scope :starts_with, -> (char) { where('name ILIKE  ?', "#{ char }%")}
+  # def self.starts_with(char)
+  #   where('name ILIKE ?', "#{ char }%")
+  # end
 
 end
